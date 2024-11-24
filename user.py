f3fbb1aa8bf9ddb34240090df1ae4752a6d276df
david1983-maker <davidkilidzan94127@gmail.com>
2024-11-24 15:00:17 +0300

    Add files via upload

--- user.py
+++ user.py
@@ -1,28 +1,22 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix='/user', tags=['user'])
-
-
-@router.get('/')
-async def all_users():
-    pass
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean,Float
+from sqlalchemy.orm import relationship
+from app.models import __init__
 
 
-@router.get('/user_id')
-async def user_by_id():
-    pass
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(Integer)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    task = relationship('Task', back_populates='user')
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
 
 
-@router.post('/create')
-async def create_user():
-    pass
 
-
-@router.put('/update')
-async def update_user():
-    pass
-
-
-@router.delete('/delete')
-async def delete_user():
-    pass
