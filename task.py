4edc8cc430a919c1e15f0f5566eb00f1345b7406
david1983-maker <davidkilidzan94127@gmail.com>
2024-11-24 14:59:06 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,22 +1,29 @@
-from app.backend.db import Base
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
-from sqlalchemy.orm import relationship
-from app.models import __init__
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/task', tags=['task'])
 
 
-class Task(Base):
-    __tablename__ = 'tasks'
-    __table_args__ = {'extend_existing': True}
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    content = Column(String)
-    priority = Column(Integer)
-    slug = Column(String, unique=True, index=True)
-    completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
-    user = relationship('User', back_populates='task')
+@router.get('/')
+async def all_tasks():
+    pass
 
 
-from sqlalchemy.schema import CreateTable
+@router.get('/task_id')
+async def task_by_id():
+    pass
 
-print(CreateTable(Task.__table__))
+
+@router.post('/create')
+async def create_task():
+    pass
+
+
+@router.put('/update')
+async def update_task():
+    pass
+
+
+@router.delete('/delete')
+async def delete_task():
+    pass
+
--- user.py
+++ user.py
@@ -1,22 +1,28 @@
-from app.backend.db import Base
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean,Float
-from sqlalchemy.orm import relationship
-from app.models import __init__
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/user', tags=['user'])
 
 
-class User(Base):
-    __tablename__ = 'users'
-    __table_args__ = {'extend_existing': True}
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(Integer)
-    age = Column(Integer)
-    slug = Column(String, unique=True, index=True)
-    task = relationship('Task', back_populates='user')
-
-from sqlalchemy.schema import CreateTable
-print(CreateTable(User.__table__))
+@router.get('/')
+async def all_users():
+    pass
 
 
+@router.get('/user_id')
+async def user_by_id():
+    pass
 
+
+@router.post('/create')
+async def create_user():
+    pass
+
+
+@router.put('/update')
+async def update_user():
+    pass
+
+
+@router.delete('/delete')
+async def delete_user():
+    pass
